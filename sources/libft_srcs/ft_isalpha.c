08836950eb708f987846539cd07a53a9b55083d1
Jiri Novotny <jnovotny@student.hive.fi>
2021-04-14 01:40:43 +0300

    isascii & isdigit

--- sources/libft_srcs/ft_isalpha.c
+++ sources/libft_srcs/ft_isalpha.c
@@ -6,11 +6,18 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 22:44:25 by jnovotny          #+#    #+#             */
-/*   Updated: 2021/04/14 01:30:47 by jnovotny         ###   ########.fr       */
+/*   Updated: 2021/04/14 01:36:58 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+/**
+ * @brief Checks if given character is alphabetical
+ * 
+ * @param c Character to test
+ * @return int 1/0 ~ true/false
+ */
 
 int	ft_isalpha(int c)
 {
--- sources/libft_srcs/ft_isdigit.c
+++ sources/libft_srcs/ft_isdigit.c
@@ -6,15 +6,20 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 22:45:37 by jnovotny          #+#    #+#             */
-/*   Updated: 2019/10/17 22:45:37 by jnovotny         ###   ########.fr       */
+/*   Updated: 2021/04/14 01:39:56 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_isdigit(int c)
+/**
+ * @brief Checks if given character is a digit
+ * 
+ * @param c Character to test
+ * @return int 1/0 ~ true/false
+ */
+
+int	ft_isdigit(int c)
 {
-	if ((c > 47 && c < 58))
-		return (1);
-	return (0);
+	return ((c > 47 && c < 58));
 }
