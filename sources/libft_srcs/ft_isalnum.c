e2aac92eb28ce74f3267a7fec5189bd7a02adc7e
Jiri Novotny <jnovotny@student.hive.fi>
2021-04-14 01:30:14 +0300

    Update ft_isalnum.c

--- sources/libft_srcs/ft_isalnum.c
+++ sources/libft_srcs/ft_isalnum.c
@@ -6,15 +6,20 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 22:44:10 by jnovotny          #+#    #+#             */
-/*   Updated: 2021/04/14 00:57:16 by jnovotny         ###   ########.fr       */
+/*   Updated: 2021/04/14 01:29:13 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/**
+ * @brief Checks if given character is alpha-numerical
+ * 
+ * @param c Checked character
+ * @return int 1 on true | 0 on false
+ */
+
 int	ft_isalnum(int c)
 {
-	if ((c > 64 && c < 91) || (c > 96 && c < 123) || (c > 47 && c < 58))
-		return (1);
-	return (0);
+	return ((c > 64 && c < 91) || (c > 96 && c < 123) || (c > 47 && c < 58));
 }
