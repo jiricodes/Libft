b16aee48fc551bdeacfb249cc9b089bd266850fc
Jiri Novotny <novotny.049@gmail.com>
2022-04-20 22:46:08 +0300

    work on norme initialized, removing register causes 50% slowdown

--- src/libc/ft_memcpy.c
+++ src/libc/ft_memcpy.c
@@ -6,12 +6,29 @@
 /*   By: jnovotny <jnovotny@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/17 22:46:14 by jnovotny          #+#    #+#             */
-/*   Updated: 2022/04/20 16:30:30 by jnovotny         ###   ########.fr       */
+/*   Updated: 2022/04/20 22:45:32 by jnovotny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "ft_libc.h"
+#include "ft_libc.h"
 
+static inline void	copy_and_slide(
+	unsigned char **dst,
+	unsigned char **src,
+	size_t len)
+{
+	register unsigned char	*dst_ptr;
+	register unsigned char	*src_ptr;
+
+	dst_ptr = *dst;
+	src_ptr = *src;
+	while (len-- > 0)
+	{
+		*dst_ptr++ = *src_ptr++;
+	}
+	*dst = dst_ptr;
+	*src = src_ptr;
+}
 
 /**
  * @brief The `ft_memcpy` function copies `n` bytes from memory area `src`
@@ -42,7 +59,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	t = (uintptr_t)src_ptr;
 	if ((t | (uintptr_t)dst_ptr) & 7)
 	{
-		if((t ^ (uintptr_t)dst_ptr) & 7 || n < 8)
+		if ((t ^ (uintptr_t)dst_ptr) & 7 || n < 8)
 			t = n;
 		else
 			t = 8 - (t & 7);
